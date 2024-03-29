c4888cb00cb1a45670e17fd834f16d81872de55c
Developer Musu <165336839+DeveloperMusu@users.noreply.github.com>
2024-03-29 17:52:13 +0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -4,10 +4,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $message = $_POST["message"];
     
-    // Process the form data (send email, save to database, etc.)
-    // For demonstration, let's just echo the data
-    echo "Name: " . $name . "<br>";
-    echo "Email: " . $email . "<br>";
-    echo "Message: " . $message;
+    // Send email
+    $to = "enderzmb@gmail.com";
+    $subject = "Message from Portfolio Website";
+    $body = "Name: $name\n\nEmail: $email\n\nMessage: $message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "<p>Your message has been sent successfully. Thank you!</p>";
+    } else {
+        echo "<p>Sorry, there was an error sending your message. Please try again later.</p>";
+    }
 }
 ?>
